838efcf3165603df6b3e9bf3b16ed61d4d942df1
Saeed <118368777+saeedhv82@users.noreply.github.com>
2023-10-13 23:03:57 +0330

    Guess my number ...
    its a game that you should guess a number that is hidden and then you can see your high score .

---
+++ Game-project1/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const secretNumber = Math.trunc (Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    // when there is no input
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!'
+        // when player wins
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!'
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    // when guess is wrong 
+    } else if (guess !== secretNumber) {
+        if(score > 0) {
+            document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 you lost the game!';
+        }
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc (Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem';
+});
